99c23f7f35676f84bfdcb36383f640c8576a52d6
Lafvin <162661251+lafvintech@users.noreply.github.com>
2025-03-03 10:46:23 +0800

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -43,8 +43,8 @@ exclude_patterns = []
 
 html_theme = 'sphinx_rtd_theme'
 
-# html_static_path = ['_static']
-# html_logo = '_static/Logo2.png'
+html_static_path = ['_static']
+html_logo = '_static/Logov2.png'
 
 html_theme_options = {
      'logo_only': True,
